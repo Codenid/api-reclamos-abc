91d6ef93743f5d38ab368f6510eee71f6af06872
Winston Codenid <winstonflores30@gmail.com>
2025-10-08 15:46:46 -0500

    Cors

--- src/api_reclamos/main.py
+++ src/api_reclamos/main.py
@@ -10,7 +10,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # o lista de dominios permitidos
+    allow_origins=[
+        "http://app-reclamos-abc.s3-website-us-east-1.amazonaws.com"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
